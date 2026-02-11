7d880ddd2947069480c3310dc00ed291afd9534c
Lexie Wen <lwen@starbucks.com>
2026-02-11 15:17:38 +0800

    chore: add and remove some comments

--- tool-registry/index.ts
+++ tool-registry/index.ts
@@ -1,11 +1,8 @@
 import { defaultToolsetManager } from '../common/toolsetManager.js';
 import { Toolset } from '../common/toolsets.js';
 
-// 保持向后兼容的接口
 export const getAllTools = () => defaultToolsetManager.getAllTools();
 
-// 新增按工具集获取工具的接口
 export const getToolsByToolset = (toolsetName: Toolset) => defaultToolsetManager.getToolsByToolset(toolsetName);
 
-// 新增获取启用工具的接口
 export const getEnabledTools = (enabledToolsets: Toolset[]) => defaultToolsetManager.getEnabledTools(enabledToolsets);
